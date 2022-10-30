8fc09829ae4c4007ca44d8bfe3c5b3f081e8f2a0
Leting Cai <letingc@nycstudents.net>
2022-10-30 17:06:13 -0400

    Third Commit

--- src/LinearEquation.java
+++ src/LinearEquation.java
@@ -1,5 +1,6 @@
 public class LinearEquation {
 
+    //Instance Variables
     private int x1;
     private int x2;
     private int y1;
@@ -7,35 +8,62 @@ public class LinearEquation {
     private String cord1;
     private String cord2;
 
-    public LinearEquation(String cord1,String cord2)
-    {
-        this.cord1=cord1;
-        this.cord2=cord2;
-        x1=Integer.parseInt(cord1.substring(1,2));
-        x2=Integer.parseInt(cord2.substring(1,2));
-        y1=Integer.parseInt(cord1.substring(3,cord1.length()-1).trim());
-        y2=Integer.parseInt(cord2.substring(3,cord2.length()-1).trim());
+    private boolean vertical;
+
+    //Constructor
+    public LinearEquation(int x1, int y1, int x2, int y2){
+        this.x1 = x1;
+        this.x2 = x2;
+        this.y1 = y1;
+        this.y2 = y2;
+        cord1 = "("+x1+", "+y1+")";
+        cord2 = "("+x2+", "+y2+")";
     }
 
-    public double slope()
-    {
-        return Math.round((y2-y1)/(x2-x1)*100)/100;
-    }
+    // distance() uses the pythagorean theorem to calculate the distance between the two coordinates.
     public double distance()
     {
-    return Math.round(Math.sqrt(Math.pow((x2-x1),2)+Math.pow((y2-y1),2))*100)/100;
+        return roundedToHundredth(Math.sqrt(Math.pow((x2-x1),2)+Math.pow((y2-y1),2)));
     }
 
+    //yIntercept() calculates the Y-Intercept by subtracting the slope multiplied by x1 from y1.
     public double yIntercept()
     {
-        return Math.round(slope()*x1-y1);
+        return roundedToHundredth(y1-slope()*x1);
+    }
+
+    //slope() calculates the slope between the two given coordinates.
+    public double slope()
+    {
+        return roundedToHundredth((double)(y2-y1)/(x2-x1));
     }
 
     public String equation()
     {
-        int deltaY=Math.abs(y2-y1);
-        int deltaX=Math.abs(x2-x1);
-        if(slope()<0) return "y=-" + deltaY + "/" + deltaX + "x"+yIntercept();
-        return "y="+deltaY+"/"+deltaX+"x"+yIntercept();
+        int deltaY=Math.abs(y2-y1);         //Set deltaY to the absolute value of the difference between y1 and y2.
+        int deltaX=Math.abs(x2-x1);         //Set deltaX to the absolute value of the difference between x2 and y1.
+        String returnString="y = ";         //First puts y = at the front of the equation.
+        if (y1==y2){return returnString+=y1;}           //If the line is horizontal then concatenate y1.
+        if(slope()<0) {returnString += " -";}           //if the slope is negative concatenate the negative sign.
+        if(slope()==1 || slope()==-1){returnString+="x";}           //If the slope is 1 or -1 only concatenate x.
+         else if(deltaY%deltaX==0){returnString += Math.abs(slope())+"x";} else {returnString+= deltaY+"/"+deltaX;}         //If the slope can be simplified use the value from slope() as the coefficient of x; If not use fractions.
+        if (yIntercept() > 0){returnString+=" + "+yIntercept();} else if (yIntercept()<0) {returnString+=" - "+Math.abs(yIntercept());}         //Only attaches the y-intercept if it is not 0.
+        return returnString;        //Return the concatenated String
+    }
+
+    //coordinateForX() calculates the Y value by substituting the xValue into the linear equation.
+    // The result is then rounded to the nearest hundredth and returned in coordinate form.
+    public String coordinateForX(double xValue){
+        return "("+xValue+", "+roundedToHundredth(xValue*slope()+yIntercept())+")";
+    }
+
+    //Rounds the parameter inputted to the nearest hundredth.
+    public double roundedToHundredth(double toRound){
+        return Math.round(toRound*100.0)/100.0;
+    }
+
+    //Returns a String that contains the equation, the two coordinates, the slope, the y-intercept, and the distance between the two points.
+    public String lineInfo(){
+        return "The two points are: " +cord1+" and "+cord2+"\nThe equation of the line between these points is: "+equation()+"\nThe slope of this line is: "+slope()+"\nThe y-intercept of this line is: "+yIntercept()+"\nThe distance between the two points is: "+distance();
     }
 }
--- src/LinearEquationRunner.java
+++ src/LinearEquationRunner.java
@@ -1,20 +1,45 @@
-import javax.sound.sampled.Line;
 import java.util.Scanner;
 public class LinearEquationRunner
 {
     public static void main(String[] args)
     {
+        //Initialize variables
         Scanner scan = new Scanner(System.in);
-        System.out.println("Enter:");
-        String x = scan.nextLine();
-        System.out.println("Again:");
-        String y = scan.nextLine();
-        LinearEquation test = new LinearEquation(x,y);
-        System.out.println(test.yIntercept());
-        System.out.println(test.distance());
-        System.out.println(test.equation());
-        System.out.println(test.slope());
+        String cord1;
+        String cord2;
+        int x1;
+        int y1;
+        int x2;
+        int y2;
+        double x;
 
+
+        //Welcomes the user and asks for the two coordinates.
+        //Uses scanners to collect user input.
+        System.out.println("Welcome!");
+        System.out.print("Enter coordinate 1: ");
+        cord1 = scan.nextLine();
+        System.out.print("Enter coordinate 2: ");
+        cord2 = scan.nextLine();
+
+        //Extract the coordinates from the string and assign them into corresponding variables.
+        x1=Integer.parseInt(cord1.substring(1,3).replaceAll(",",""));
+        x2=Integer.parseInt(cord2.substring(1,3).replaceAll(",",""));
+        y1=Integer.parseInt(cord1.substring(3,cord1.length()-1).replaceAll(",","").trim());
+        y2=Integer.parseInt(cord2.substring(3,cord2.length()-1).replaceAll(",","").trim());
+
+        //Checks if the line is vertical; if not create a new LinearEquation object obj.
+        if (x1==x2){System.out.println("\nThese points are on a vertical line: x = "+x1); return;}
+        LinearEquation obj = new LinearEquation(x1,y1,x2,y2);
+
+        //Calls the method lineInfo() and prints it out.
+        System.out.println("\n"+obj.lineInfo());
+
+        //Asks for X value.
+        //Use scanner to collect user input.
+        //Then calculate the corresponding Y value by calling the method coordinateForX()
+        System.out.print("\nEnter a value for X: ");
+        x =  scan.nextDouble();
+        System.out.println("\nThe point on the line is " + obj.coordinateForX(x));
     }
-
 }
